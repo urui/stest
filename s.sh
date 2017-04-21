cf3d253a3aa875a06e672aa81788c092deca4971
urui <daisy.hello.world@gmail.com>
2017-04-21 15:46:48 +0900

    hensuu

--- s.sh
+++ s.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo "\$0はスクリプト名 : $0"
-echo "\$1は一番目の引数 : $1"
-echo "\$2は二番目の引数 : $2"
-echo "\$#は引数の数 : $# "
-echo "\"\$*\": \"$*\" "
-echo "\"\$@\": \"$@\""
-echo "exit値は0になるはず"
-echo $?
+echo "1:${var:-変数がセットされていないか空の時にwordを返します。varに保存しない。}"
+echo "2: var=${var}"
+echo "3:${var:=変数がセットされていないか空の時にwordを返します。varに保存する。}"
+echo "4: var = ${var}"
+unset var
+echo "5:${var:+変数がセットされている時にwordを返し、varに保存しない。}"
+echo "6:var= ${var}"
+var="atai"
+echo "7:${var:+変数がセットされている時にwordを返し、varに保存しない。}"
+echo "8:var= ${var}"
+echo "9: ${var:?エラー}"
+echo "10: var= ${var}"
